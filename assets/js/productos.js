6af2703a92aeefa9d79232413890272b4da7326f
DarienIgnacio <dari.urrutia@duocuc.cl>
2026-09-09 20:35:26 -0300

    Carga de productos
    Se carga una lista de productos con sus respectivas imagenes y se le agrega su propio estilo en el css

---
+++ assets/js/productos.js
@@ -0,0 +1,42 @@
+const productosIniciales = [
+  {
+    codigo: "MC001",
+    nombre: "Cemento Polpaico gris 25 kg",
+    categoria: "Mat. Construcción",
+    precio: 5990,
+    stock: 80,
+    stockCritico: 20,
+    imagen: "assets/img/cemento-polpaico.png",
+    descripcion: "Cemento tradicional para radieres, pilares y albañilería general."
+  },
+  {
+    codigo: "MC002",
+    nombre: "Cemento Melón blanco 25 kg",
+    categoria: "Mat. Construcción",
+    precio: 7490,
+    stock: 4,
+    stockCritico: 10,
+    imagen: "assets/img/cemento-melon.png",
+    descripcion: "Cemento blanco de alta resistencia para acabados finos."
+  },
+  {
+    codigo: "MC003",
+    nombre: "Mortero cola cerámica 25 kg",
+    categoria: "Mat. Construcción",
+    precio: 5200,
+    stock: 50,
+    stockCritico: 15,
+    imagen: "assets/img/mortero-cola.png",
+    descripcion: "Adhesivo en polvo listo para pegar cerámicas en interiores."
+  },
+  {
+    codigo: "MC007",
+    nombre: "Ladrillo fiscal N°5",
+    categoria: "Mat. Construcción",
+    precio: 380,
+    stock: 0,
+    stockCritico: 100,
+    imagen: "assets/img/ladrillo-fiscal.png",
+    descripcion: "Ladrillo artesanal para muros estructurados."
+  }
+];
